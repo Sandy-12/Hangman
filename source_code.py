b033a4e8d9c026ae6c97dec040a505e0637ffb2b
Sandy-12 <sandipan.saha@st.niituniversity.in>
2016-08-16 23:52:38 +0530

    Create source_code.py

---
+++ source_code.py
@@ -0,0 +1,5 @@
+# Hangman
+
+# Group members: Amrit Bagga (ECE- S6), Amruta Anand Kedia (ECE- S6), Samarth Gupta (ECE- S6), Sandipan Saha (BT- S6)
+
+# The game takes choice of word size, max character help which will be displayed at random positions in the word. This program would need a text file containing words of different lengths which would be picked up as per the word size specified by the person playing the game. After every wrong guess the program would display the word with hyphens at the characters that are not disclosed and one new character would be disclosed each time till the max tries.
